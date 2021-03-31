3a12439d70609e7036466f6f40e49f60f3648577
reemsh2000 <sh951652@gmail.com>
2021-03-31 16:29:33 +0300

    git element

---
+++ index.js
@@ -0,0 +1,8 @@
+let search_btn=document.querySelector(".searchBtn");
+let input=document.getElementById("userInput");
+let img1=document.getElementById('first-cat');
+let img1=document.getElementById('second-cat');
+let img1=document.getElementById('third-cat');
+let img1=document.getElementById('forth-cat');
+search_btn.addEventListener('click',getphoto);
+//fetch(https://api.thecatapi.com/v1/images/search??breed_ids=jgjhgj&limit=5) 
